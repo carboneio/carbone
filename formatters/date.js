9e45d22c250348f42ced0529fbb953b6aa805930
David Grelaud <dgrelaud@ideolys.com>
2016-12-01 19:01:30 +0100

    Fix: in formatters convert, format, addDays, parse: if the date is null or undefined these formatters return null or undefined instead of "Invalid Date"

--- formatters/date.js
+++ formatters/date.js
@@ -1,37 +1,59 @@
 var moment = require('moment');
 
-module.exports = {
-  /**
-   * Converts a date from a pattern to another pattern
-   * @param  {String} patternIn
-   * @param  {String} patternOut
-   * @return {String}
-   */
-  convert : function(d, patternIn, patternOut){
+
+/**
+ * Format dates
+ * @param  {String|Number} d    date to format
+ * @param  {String} patternIn  input format 
+ * @param  {String} patternOut output format
+ * @return {String}            return formatted date
+ */
+function convDate(d, patternIn, patternOut){
+  if(d !== null && typeof d !== 'undefined'){
+    //moment.locale(this.lang);
     return moment(d + '', patternIn).format(patternOut);
-  },
+  }
+  return d;
+}
+
+
+module.exports = {
+  convDate : convDate,
+  convert : convDate,
+
   /**
    * Adds a number of days to the date
    * @param  {Integer} nbDay: the number of days to add
    * @return {String}
    */
   addDays : function(d, nbDay){
-    return moment(d).add('days', parseInt(nbDay));
+    if(d !== null && typeof d !== 'undefined'){
+      return moment(d).add('days', parseInt(nbDay));
+    }
+    return d;
   },
   /**
    * Converts a date to a string with given pattern
+   * DEPRECATED
    * @param  {String} pattern
    * @return {String}
    */
   format : function(d, pattern){
-    return moment(d).format(pattern);
+    if(d !== null && typeof d !== 'undefined'){
+      return moment(d).format(pattern);
+    }
+    return d;
   },
   /**
    * Converts a string with given pattern to date
+   * DEPRECATED
    * @param  {String} pattern
    * @return {Date}
    */
   parse : function(d, pattern){
-    return moment(d + '', pattern).toDate();
+    if(d !== null && typeof d !== 'undefined'){
+      return moment(d + '', pattern).toDate();
+    }
+    return d;
   }
 };

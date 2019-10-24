fa67247719c0e50a2c428da6fa3d707a51c436f7
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2019-10-24 06:27:09 +0200

    Unit tests cleaner and better

--- test/test.postprocessor.js
+++ test/test.postprocessor.js
@@ -6,102 +6,43 @@ var path = require('path');
 
 describe('postprocessor', function () {
   describe('File type checking', function () {
-    it('Throw an error when the file type is not defined on the filename extension and the option object.', function (done) {
-      const template = {
-        isZipped   : true,
-        filename   : './template',
-        embeddings : [],
-        files      :
-         [ { name : 'META-INF/manifest.xml',
-           data :
-              '<?xml version="1.0" encoding="UTF-8"?>',
-           isMarked : true,
-           parent   : '' } ],
-        reportName : undefined,
-        extension  : ''
-      };
-      const data = { firstname : 'John' };
-      const options = {
-        lang         : 'en',
-        translations : {},
-        complement   : undefined,
-        reportName   : undefined,
-        convertTo    : 'pdf',
-        extension    : '',
-        formatters   :
-         {
-         },
-        existingVariables : []
-      };
-      postprocessor.execute(template, data, options, function (err, response) {
+    const template = {
+      filename   : '',
+      embeddings : [],
+      files      :
+       [ { name     : 'META-INF/manifest.xml',
+         data     : '<?xml version="1.0" encoding="UTF-8"?>',
+         isMarked : true,
+         parent   : '' } ],
+      extension : ''
+    };
+    const options = {
+      extension : '',
+    };
+    it('Throw an error when the file type is not defined on the filename extension and the options object.', function (done) {
+      template.filename = './template';
+      options.extension = '',
+      postprocessor.execute(template, {}, options, function (err, response) {
         helper.assert(/Error: The file type is not defined on the filename extension or on the option object.*/.test(err), true);
         helper.assert(undefined, response);
         done();
       });
     });
 
-    it('Return a valid template through the file type defined on the file name extension', function (done) {
-      const template = {
-        isZipped   : true,
-        filename   : './template.ods',
-        embeddings : [],
-        files      :
-         [ { name : 'META-INF/manifest.xml',
-           data :
-              '<?xml version="1.0" encoding="UTF-8"?>',
-           isMarked : true,
-           parent   : '' } ],
-        reportName : undefined,
-        extension  : ''
-      };
-      const data = { firstname : 'John' };
-      const options = {
-        lang         : 'en',
-        translations : {},
-        complement   : undefined,
-        reportName   : undefined,
-        convertTo    : '',
-        extension    : '',
-        formatters   :
-         {
-         },
-        existingVariables : []
-      };
-      postprocessor.execute(template, data, options, function (err, response) {
+    it('Return a valid template if the file type is defined on the file name extension', function (done) {
+      template.filename = './template.ods';
+      options.extension = '',
+      postprocessor.execute(template, {}, options, function (err, response) {
         helper.assert(err, null);
         helper.assert(JSON.stringify(template), JSON.stringify(response));
         done();
       });
     });
 
-    it('Return a valid template through the file type defined on the option object.', function (done) {
-      const template = {
-        isZipped   : true,
-        filename   : './template',
-        embeddings : [],
-        files      :
-         [ { name : 'META-INF/manifest.xml',
-           data :
-              '<?xml version="1.0" encoding="UTF-8"?>',
-           isMarked : true,
-           parent   : '' } ],
-        reportName : undefined,
-        extension  : ''
-      };
-      const data = { firstname : 'John' };
-      const options = {
-        lang         : 'en',
-        translations : {},
-        complement   : undefined,
-        reportName   : undefined,
-        convertTo    : 'ods',
-        extension    : 'ods',
-        formatters   :
-         {
-         },
-        existingVariables : []
-      };
-      postprocessor.execute(template, data, options, function (err, response) {
+    it('Return a valid template if the file type is defined on the options object.', function (done) {
+      template.filename = './template';
+      options.extension = 'ods',
+      postprocessor.execute(template, {}, options, function (err, response) {
         helper.assert(err, null);
         helper.assert(JSON.stringify(template), JSON.stringify(response));
         done();

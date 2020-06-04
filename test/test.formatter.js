58fc08b6638dec6d54f108f6853d9d90f222ac7a
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-06-04 11:10:12 +0200

    Added test -  IfNE

--- test/test.formatter.js
+++ test/test.formatter.js
@@ -244,23 +244,21 @@ describe('formatter', function () {
       callWithContext(conditionFormatter.ifEQ, _context, null, null);
       helper.assert(_context.isConditionTrue, true);
       helper.assert(_context.stopPropagation, false);
-      // object identiques
+      // object identiques, it is comparing "[object Object]" === "[object Object]"
+      _context.isConditionTrue = false;
       callWithContext(conditionFormatter.ifEQ, _context, {value : 10, name : 'john', address : { name : '123 street'} }, {value : 10, name : 'john', address : { name : '123 street'}});
       helper.assert(_context.isConditionTrue, true);
       helper.assert(_context.stopPropagation, false);
-      // object différents
+      // different objects, it is comparing "[object Object]" === "[object Object]"
+      _context.isConditionTrue = false;
       callWithContext(conditionFormatter.ifEQ, _context, {value : 20, name : 'Eric', address : { name : '85 street'} }, {value : 10, name : 'wick', address : { name : '321 street'}});
       helper.assert(_context.isConditionTrue, true);
       helper.assert(_context.stopPropagation, false);
       // Tableau identique
+      _context.isConditionTrue = false;
       callWithContext(conditionFormatter.ifEQ, _context, [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7, 8, 9]);
       helper.assert(_context.isConditionTrue, true);
       helper.assert(_context.stopPropagation, false);
-      // Tableau différents
-      callWithContext(conditionFormatter.ifEQ, _context, [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 3, 6, 7, 8, 9]);
-      helper.assert(_context.isConditionTrue, true);
-      helper.assert(_context.stopPropagation, false);
-
     });
 
     it('should turn the `isConditionTrue` to false if the data is not equal to a variable', function () {
@@ -289,8 +287,106 @@ describe('formatter', function () {
       callWithContext(conditionFormatter.ifEQ, _context, undefined, 'titi');
       helper.assert(_context.isConditionTrue, false);
       helper.assert(_context.stopPropagation, false);
+      _context.isConditionTrue = false;
       callWithContext(conditionFormatter.ifEQ, _context, 'titi', null);
       helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      _context.isConditionTrue = false;
+      callWithContext(conditionFormatter.ifEQ, _context, [1, 2, 3], [1, 3]);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+    });
+  });
+
+  describe.only('ifNE', function () {
+    it('should turn the `isConditionTrue` to false if a data is equal to a variable', function () {
+      var _context = {};
+      callWithContext(conditionFormatter.ifNE, _context, 0, 0);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 0, '0');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, true, true);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'true', 'true');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, true, 'true');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, false, 'false');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'false', 'false');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, false, false);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, false, 'false');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'titi', 'titi');
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, undefined, undefined);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, null, null);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      // object identiques
+      _context.isConditionTrue = false;
+      callWithContext(conditionFormatter.ifNE, _context, {value : 10, name : 'john', address : { name : '123 street'} }, {value : 10, name : 'john', address : { name : '123 street'}});
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      // object différents
+      _context.isConditionTrue = false;
+      callWithContext(conditionFormatter.ifNE, _context, {value : 20, name : 'Eric', address : { name : '85 street'} }, {value : 10, name : 'wick', address : { name : '321 street'}});
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+      // Tableau identique
+      _context.isConditionTrue = false;
+      callWithContext(conditionFormatter.ifNE, _context, [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7, 8, 9]);
+      helper.assert(_context.isConditionTrue, false);
+      helper.assert(_context.stopPropagation, false);
+    });
+
+    it('should turn the `isConditionTrue` to false if the data is not equal to a variable', function () {
+      var _context = {};
+      callWithContext(conditionFormatter.ifNE, _context, 0, 3);
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 0, '1');
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, true, false);
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'true', false);
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'false', 'true');
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, false, true);
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'titi', 'toto');
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, undefined, 'titi');
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      callWithContext(conditionFormatter.ifNE, _context, 'titi', null);
+      helper.assert(_context.isConditionTrue, true);
+      helper.assert(_context.stopPropagation, false);
+      // Tableau différents
+      _context.isConditionTrue = false;
+      callWithContext(conditionFormatter.ifNE, _context, [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 3, 6, 7, 8, 9]);
+      helper.assert(_context.isConditionTrue, true);
       helper.assert(_context.stopPropagation, false);
     });
   });

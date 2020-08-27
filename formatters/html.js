708bc963c64afea4e68daac4637cbe85254b6207
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-08-27 16:33:38 +0200

    Added html pre processing + formatter processing

---
+++ formatters/html.js
@@ -0,0 +1,61 @@
+const html = require('../lib/html');
+
+/**
+ * Add the color to the htmlDatabase Map.
+ *
+ * @private
+ *
+ * @param  {Object} options
+ * @param  {Array} color
+ */
+function addHtmlDatabase (options, htmlContent, styleName) {
+  var _htmlDatabaseProperties = null;
+
+  if (!options.htmlDatabase.has(htmlContent)) {
+    const { styleList, content } = html.parseStyleAndGetStyleList(htmlContent);
+    _htmlDatabaseProperties = {
+      id : options.htmlDatabase.size,
+      styleName,
+      content,
+      styleList
+    };
+    options.htmlDatabase.set(htmlContent, _htmlDatabaseProperties);
+    console.log(options.htmlDatabase);
+  }
+}
+
+
+const getHtmlContent = function (htmlContent, styleName) {
+  addHtmlDatabase(this, htmlContent, styleName);
+  return {
+    fn   : getHtmlContentPostProcess,
+    args : [htmlContent]
+  };
+};
+
+const getHtmlContentPostProcess = function (htmlContentId) {
+  const _htmlProperties = this.htmlDatabase.get(htmlContentId);
+  console.log(_htmlProperties.content);
+  return _htmlProperties.content;
+};
+
+const getHtmlStyleName = function (htmlContent, styleName) {
+  addHtmlDatabase(this, htmlContent, styleName);
+  return {
+    fn   : getHtmlStyleNamePostProcess,
+    args : [htmlContent]
+  };
+};
+
+const getHtmlStyleNamePostProcess = function (htmlContentId) {
+  const _htmlProperties = this.htmlDatabase.get(htmlContentId);
+  console.log(html.generateStyleName(_htmlProperties.id));
+  return html.generateStyleName(_htmlProperties.id);
+};
+
+module.exports = {
+  getHtmlContent,
+  getHtmlContentPostProcess,
+  getHtmlStyleName,
+  getHtmlStyleNamePostProcess
+};

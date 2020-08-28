0c7be526157b448261229026646b21992b7bac06
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-08-28 09:20:07 +0200

    Insert span tag to every marker

--- formatters/html.js
+++ formatters/html.js
@@ -8,45 +8,47 @@ const html = require('../lib/html');
  * @param  {Object} options
  * @param  {Array} color
  */
-function addHtmlDatabase (options, htmlContent, styleName) {
+function addHtmlDatabase (options, contentId, htmlContent, styleFamily) {
   var _htmlDatabaseProperties = null;
 
-  if (!options.htmlDatabase.has(htmlContent)) {
+  if (!options.htmlDatabase.has(contentId)) {
     const { styleList, content } = html.parseStyleAndGetStyleList(htmlContent);
     _htmlDatabaseProperties = {
       id : options.htmlDatabase.size,
-      styleName,
+      styleFamily,
       content,
       styleList
     };
-    options.htmlDatabase.set(htmlContent, _htmlDatabaseProperties);
+    options.htmlDatabase.set(contentId, _htmlDatabaseProperties);
   }
 }
 
 
-const getHtmlContent = function (htmlContent, styleName) {
-  addHtmlDatabase(this, htmlContent, styleName);
+const getHtmlContent = function (htmlContent, styleFamily) {
+  const _contentId = htmlContent + styleFamily;
+  addHtmlDatabase(this, _contentId, htmlContent, styleFamily);
   return {
     fn   : getHtmlContentPostProcess,
-    args : [htmlContent]
+    args : [_contentId]
   };
 };
 
-const getHtmlContentPostProcess = function (htmlContentId) {
-  const _htmlProperties = this.htmlDatabase.get(htmlContentId);
+const getHtmlContentPostProcess = function (contentId) {
+  const _htmlProperties = this.htmlDatabase.get(contentId);
   return _htmlProperties.content;
 };
 
-const getHtmlStyleName = function (htmlContent, styleName) {
-  addHtmlDatabase(this, htmlContent, styleName);
+const getHtmlStyleName = function (htmlContent, styleFamily) {
+  const _contentId = htmlContent + styleFamily;
+  addHtmlDatabase(this, _contentId, htmlContent, styleFamily);
   return {
     fn   : getHtmlStyleNamePostProcess,
-    args : [htmlContent]
+    args : [_contentId]
   };
 };
 
-const getHtmlStyleNamePostProcess = function (htmlContentId) {
-  const _htmlProperties = this.htmlDatabase.get(htmlContentId);
+const getHtmlStyleNamePostProcess = function (contentId) {
+  const _htmlProperties = this.htmlDatabase.get(contentId);
   return html.generateStyleName(_htmlProperties.id);
 };
 

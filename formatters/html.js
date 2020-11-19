28cf57d5b164d1d1d03152a9cfe9e5ccf31f821d
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-11-19 18:20:06 +0100

    Simple replacement

--- formatters/html.js
+++ formatters/html.js
@@ -1,5 +1,7 @@
 const html = require('../lib/html');
 
+/** ======================= ODT ======================== */
+
 /**
  * Add the content to the htmlDatabase Map.
  *
@@ -49,9 +51,88 @@ const getHtmlStyleNamePostProcess = function (contentId) {
   return html.generateStyleName(_htmlProperties.id);
 };
 
+/** ======================= DOCX ======================== */
+
+/**
+ * Add the content to the htmlDatabase Map.
+ *
+ * @private
+ *
+ * @param  {Object} options contains htmlDatabase
+ * @param  {String} htmlContent string with html tags
+ */
+function addHtmlDatabaseDOCX (options, htmlContent) {
+  // var _htmlDatabaseProperties = null;
+
+  if (!options.htmlDatabase.has(htmlContent)) {
+    // const { styleList, content } = html.parseStyleAndGetStyleList(htmlContent);
+    // console.log(content, styleList);
+    // _htmlDatabaseProperties = {
+    //   id : options.htmlDatabase.size,
+    //   content,
+    //   styleList
+    // };
+    // options.htmlDatabase.set(htmlContent, _htmlDatabaseProperties);
+  }
+}
+
+const getHTMLSubContentDocx = function (htmlContent) {
+  addHtmlDatabaseDOCX(this, htmlContent);
+
+  return {
+    fn   : getHTMLSubContentDocxPostProcess,
+    args : []
+  };
+};
+
+const getHTMLSubContentDocxPostProcess = function () {
+  /**
+   * `<w:br/>
+  <w:r>
+  <w:rPr>
+    <w:lang w:val="en-US"/>
+  </w:rPr>
+  <w:t xml:space="preserve"> Pouette pouette </w:t>
+  </w:r>`
+   */
+  return '';
+};
+
+const getHTMLContentDocx = function (htmlContent) {
+  addHtmlDatabaseDOCX(this, htmlContent);
+  return {
+    fn   : getHTMLContentDocxPostProcess,
+    args : [htmlContent]
+  };
+};
+
+const getHTMLContentDocxPostProcess = function (contentId) {
+  const _htmlProperties = this.htmlDatabase.get(contentId);
+  return _htmlProperties.content;
+};
+
+const getHTMLContentStyleDocx = function (htmlContent) {
+  addHtmlDatabaseDOCX(this, htmlContent);
+  return {
+    fn   : getHTMLContentStyleDocxPostProcess,
+    args : [htmlContent]
+  };
+};
+
+const getHTMLContentStyleDocxPostProcess = function (contentId) {
+  const _htmlProperties = this.htmlDatabase.get(contentId);
+  return _htmlProperties.styleList;
+};
+
 module.exports = {
   getHtmlContent,
   getHtmlContentPostProcess,
   getHtmlStyleName,
-  getHtmlStyleNamePostProcess
+  getHtmlStyleNamePostProcess,
+  html : function () {
+    return '';
+  },
+  getHTMLSubContentDocx,
+  getHTMLContentDocx,
+  getHTMLContentStyleDocx
 };

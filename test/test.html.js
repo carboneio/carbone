c987e06ee29ec971a533e0748e6af2f06d53e952
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-11-26 11:06:43 +0100

    Unit test pre processing DOCX HTML done

--- test/test.html.js
+++ test/test.html.js
@@ -253,10 +253,261 @@ describe.only('Dynamic HTML', function () {
       });
     });
   });
-  describe('Docx Documents', function () {
+  describe.only('Docx Documents', function () {
+    describe('preProcessDocx', function () {
+      it('should do nothing is the content does not contain HTML markers', function () {
+        const _expectedContent = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><w:document><w:body><w:r><w:rPr><w:lang w:val="en-US"/></w:rPr><w:t>Some text 12345</w:t></w:r></w:p><w:sectPr w:rsidR="002B2418" w:rsidRPr="00ED3CF0"><w:pgSz w:w="11906" w:h="16838"/><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="708" w:footer="708" w:gutter="0"/><w:cols w:space="708"/><w:docGrid w:linePitch="360"/></w:sectPr></w:body></w:document>';
+        const _expectedApp = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"><Template>Normal.dotm</Template><TotalTime>6</TotalTime><Pages>1</Pages><Words>4</Words><Characters>26</Characters><Application>Microsoft Office Word</Application><DocSecurity>0</DocSecurity><Lines>1</Lines><Paragraphs>1</Paragraphs><ScaleCrop>false</ScaleCrop><HeadingPairs><vt:vector size="2" baseType="variant"><vt:variant><vt:lpstr>Title</vt:lpstr></vt:variant><vt:variant><vt:i4>1</vt:i4></vt:variant></vt:vector></HeadingPairs><TitlesOfParts><vt:vector size="1" baseType="lpstr"><vt:lpstr></vt:lpstr></vt:vector></TitlesOfParts><Company></Company><LinksUpToDate>false</LinksUpToDate><CharactersWithSpaces>29</CharactersWithSpaces><SharedDoc>false</SharedDoc><HyperlinksChanged>false</HyperlinksChanged><AppVersion>16.0000</AppVersion></Properties>';
+        const _expectedSettings = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <w:webSettings xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:w16cex="http://schemas.microsoft.com/office/word/2018/wordml/cex" xmlns:w16cid="http://schemas.microsoft.com/office/word/2016/wordml/cid" xmlns:w16="http://schemas.microsoft.com/office/word/2018/wordml" xmlns:w16se="http://schemas.microsoft.com/office/word/2015/wordml/symex" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex"><w:optimizeForBrowser/><w:allowPNG/></w:webSettings>';
+        const _expectedRels = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId7" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/header" Target="header1.xml"/><Relationship Id="rId9" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/footer" Target="footer1.xml"/></Relationships>';
+        const _template = {
+          files : [
+            {
+              name : 'word/document.xml',
+              data : _expectedContent
+            },
+            {
+              name : 'docProps/app.xml',
+              data : _expectedApp,
+            },
+            {
+              name : 'word/webSettings.xml',
+              data : _expectedSettings,
+            },
+            {
+              name : 'word/_rels/document.xml.rels',
+              data : _expectedRels,
+            }
+          ]
+        };
+        html.preProcessDocx(_template);
+        helper.assert(_template.files[0].data, _expectedContent);
+        helper.assert(_template.files[1].data, _expectedApp);
+        helper.assert(_template.files[2].data, _expectedSettings);
+        helper.assert(_template.files[3].data, _expectedRels);
+      });
 
+      it('should find one HTML formatter and inject HTML formatters for: style, content, subContent and spacePreserve', function () {
+        const _XMLtemplate = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '</w:rPr>' +
+                  '<w:t>{d.mix1:html}</w:t>' +
+                '</w:r>' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _XMLexpected = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '{d.mix1:getHTMLContentStyleDocx}</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.mix1:getHTMLContentDocx}</w:t>' +
+                '</w:r>{d.mix1:getHTMLSubContentDocx}' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _template = {
+          files : [
+            {
+              name : 'word/document.xml',
+              data : _XMLtemplate,
+            }
+          ]
+        };
+        html.preProcessDocx(_template);
+        helper.assert(_template.files[0].data, _XMLexpected);
+      });
+
+      it('should find 3 HTML formatter and inject HTML formatters for: style, content, subContent and spacePreserve', function () {
+        const _XMLtemplate = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '</w:rPr>' +
+                  '<w:t>{d.mix1:html}</w:t>' +
+                '</w:r>' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '</w:rPr>' +
+                  '<w:t>{d.strong:html}</w:t>' +
+                '</w:r>' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '</w:rPr>' +
+                  '<w:t>{d.italic:html}</w:t>' +
+                '</w:r>' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _XMLexpected = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '{d.mix1:getHTMLContentStyleDocx}</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.mix1:getHTMLContentDocx}</w:t>' +
+                '</w:r>{d.mix1:getHTMLSubContentDocx}' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '{d.strong:getHTMLContentStyleDocx}</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.strong:getHTMLContentDocx}</w:t>' +
+                '</w:r>{d.strong:getHTMLSubContentDocx}' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '{d.italic:getHTMLContentStyleDocx}</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.italic:getHTMLContentDocx}</w:t>' +
+                '</w:r>{d.italic:getHTMLSubContentDocx}' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _template = {
+          files : [
+            {
+              name : 'word/document.xml',
+              data : _XMLtemplate,
+            }
+          ]
+        };
+        html.preProcessDocx(_template);
+        helper.assert(_template.files[0].data, _XMLexpected);
+      });
+
+      it('should find one HTML formatter and inject HTML formatters for: style, content, subContent and WITHOUT spacePreserve (already inserted)', function () {
+        const _XMLtemplate = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.strongContent:html}</w:t>' +
+                '</w:r>' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _expectedXMLTemplate = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:document>' +
+            '<w:body>' +
+              '<w:p >' +
+                '<w:r>' +
+                  '<w:rPr>' +
+                    '<w:lang w:val="en-US"/>' +
+                  '{d.strongContent:getHTMLContentStyleDocx}</w:rPr>' +
+                  '<w:t xml:space="preserve">{d.strongContent:getHTMLContentDocx}</w:t>' +
+                '</w:r>{d.strongContent:getHTMLSubContentDocx}' +
+              '</w:p>' +
+            '</w:body>' +
+          '</w:document>';
+        const _XMLfooter = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:ftr>' +
+            '<w:p>' +
+              '<w:pPr>' +
+                '<w:pStyle w:val="Footer"/>' +
+                '<w:jc w:val="right"/>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '</w:rPr>' +
+              '</w:pPr>' +
+              '<w:r>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '</w:rPr>' +
+                '<w:t>{d.strikedel:html}</w:t>' +
+              '</w:r>' +
+            '</w:p>' +
+          '</w:ftr>';
+        const _expectedXMLfooter = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:ftr>' +
+            '<w:p>' +
+              '<w:pPr>' +
+                '<w:pStyle w:val="Footer"/>' +
+                '<w:jc w:val="right"/>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '</w:rPr>' +
+              '</w:pPr>' +
+              '<w:r>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '{d.strikedel:getHTMLContentStyleDocx}</w:rPr>' +
+                '<w:t xml:space="preserve">{d.strikedel:getHTMLContentDocx}</w:t>' +
+              '</w:r>{d.strikedel:getHTMLSubContentDocx}' +
+            '</w:p>' +
+          '</w:ftr>';
+        const _XMLheader = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:hdr>' +
+            '<w:p>' +
+              '<w:r>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '</w:rPr>' +
+                '<w:t>{d.italic:html}</w:t>' +
+              '</w:r>' +
+            '</w:p>' +
+          '</w:hdr>';
+        const _expectedXMLheader = '' +
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+          '<w:hdr>' +
+            '<w:p>' +
+              '<w:r>' +
+                '<w:rPr>' +
+                  '<w:lang w:val="en-US"/>' +
+                '{d.italic:getHTMLContentStyleDocx}</w:rPr>' +
+                '<w:t xml:space="preserve">{d.italic:getHTMLContentDocx}</w:t>' +
+              '</w:r>{d.italic:getHTMLSubContentDocx}' +
+            '</w:p>' +
+          '</w:hdr>';
+        const _template = {
+          files : [
+            {
+              name : 'word/document.xml',
+              data : _XMLtemplate,
+            },
+            {
+              name : 'word/footer1.xml',
+              data : _XMLfooter
+            },
+            {
+              name : 'word/header1.xml',
+              data : _XMLheader
+            }
+          ]
+        };
+        html.preProcessDocx(_template);
+        helper.assert(_template.files[0].data, _expectedXMLTemplate);
+        helper.assert(_template.files[1].data, _expectedXMLfooter);
+        helper.assert(_template.files[2].data, _expectedXMLheader);
+      });
+    });
   });
-  describe.only('Utils', function () {
+  describe('Utils', function () {
     describe('parseHTML', function () {
       it('should parse HTML content and return a descriptors [SIMPLE]', function () {
         helper.assert(html.parseHTML('This is a simple text'), [ { content : 'This is a simple text', tags : [] } ]);

5ee311d051ae559643cff2b41e177d3634936620
David Grelaud <david@carbone.io>
2021-12-09 00:12:11 +0100

    add one test

--- test/test.color.js
+++ test/test.color.js
@@ -1111,11 +1111,20 @@ describe('Dynamic colors', function () {
             data : parser.removeXMLInsideMarkers('<w:document><w:body><w:tbl><w:tr><w:trPr></w:trPr><w:tc><w:tcPr><w:tcW w:w="9972" w:type="dxa"/><w:shd w:fill="FF0000" w:val="clear" w:themeFill="accent6"/></w:tcPr></w:tc></w:tr></w:tbl><w:p><w:r><w:t>{bindColor(#ff0000, #hexa)=d.color}</w:t></w:r></w:p></w:body></w:document>')
           }]
         };
+        // should not remove themeFill if there is no binding
+        const _template3 = {
+          files : [{
+            name : 'word/document.xml',
+            data : parser.removeXMLInsideMarkers('<w:document><w:body><w:tbl><w:tr><w:trPr></w:trPr><w:tc><w:tcPr><w:tcW w:w="9972" w:type="dxa"/><w:shd w:fill="000000" w:val="clear" w:themeFill="accent6"/></w:tcPr></w:tc></w:tr></w:tbl><w:p><w:r><w:t>{bindColor(#ff0000, #hexa)=d.color}</w:t></w:r></w:p></w:body></w:document>')
+          }]
+        };
         const _expectedXML = '<w:document><w:body><w:tbl><w:tr><w:trPr></w:trPr><w:tc><w:tcPr><w:tcW w:w="9972" w:type="dxa"/><w:shd w:fill="{d.color:getAndConvertColorDocx(#hexa, textColor)}" w:val="clear"/></w:tcPr></w:tc></w:tr></w:tbl><w:p><w:r><w:t></w:t></w:r></w:p></w:body></w:document>';
         color.preProcessDocx(_template);
         helper.assert(_template.files[0].data, _expectedXML);
         color.preProcessDocx(_template2);
         helper.assert(_template2.files[0].data, _expectedXML);
+        color.preProcessDocx(_template3);
+        helper.assert(_template3.files[0].data, '<w:document><w:body><w:tbl><w:tr><w:trPr></w:trPr><w:tc><w:tcPr><w:tcW w:w="9972" w:type="dxa"/><w:shd w:fill="000000" w:val="clear" w:themeFill="accent6"/></w:tcPr></w:tc></w:tr></w:tbl><w:p><w:r><w:t></w:t></w:r></w:p></w:body></w:document>');
       });
 
       it('should replace mutliple cells color with color markers + formatters', function () {

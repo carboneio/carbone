69cca0aa92f0dc5df39643356c6b01678bf8bc52
David Grelaud <david@carbone.io>
2023-08-04 11:06:55 +0200

    add test

--- test/test.chart.js
+++ test/test.chart.js
@@ -424,6 +424,19 @@ describe('chart', function () {
         done();
       });
     });
+    it('should not break reports with two charts, if <draw:object is not a self-closing tag', function (done) {
+      const _data = [
+        { label : 'row1' , valCol1 : 10 , valCol2 : 100.1 },
+        { label : 'row2' , valCol1 : 20 , valCol2 : 200.2 },
+        { label : 'row3' , valCol1 : 30 , valCol2 : 300.3 }
+      ];
+      const _testedReport = 'chart/odp-two-charts';
+      carbone.render(helperTest.openTemplate(_testedReport), _data, (err, res) => {
+        helperTest.assert(err+'', 'null');
+        helperTest.assertFullReport(res, _testedReport);
+        done();
+      });
+    });
   });
 
   describe('[Full test] PPTX', function () {
